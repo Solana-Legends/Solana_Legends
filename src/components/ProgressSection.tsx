d2bed96504be7885f10dee57b045393419069059
Manuel <samusnow@hotmail.com>
2025-12-01 03:35:50 +0100

    Prueba

--- src/components/ProgressSection.tsx
+++ src/components/ProgressSection.tsx
@@ -110,14 +110,14 @@ export default function ProgressSection() {
       {/* ⚡ Imagen del héroe ZapSol alineada a la derecha de las barras de progreso */}
       <div className="mt-[-9rem] mb-8 flex justify-end relative animate-in fade-in slide-in-from-right duration-1000 mr-[-8rem] translate-x-[-2rem]">
           <div className="absolute inset-0 flex justify-end items-center pointer-events-none z-0">
-            <div className="w-48 h-48 rounded-full blur-3xl aura-pulsante-gold-strong"></div>
+            <div className="w-80 h-80 rounded-full blur-3xl aura-pulsante-gold-strong"></div>
           </div>
           <a>
         {/* Imagen del héroe */}
         <img
           src="/assets/ZapSol.png"
           alt="ZapSol Hero"
-          className="h-24 w-auto md:h-32 object-contain mix-blend-overlay opacity-90 logo-respirando"
+          className="h-64 w-auto md:h-72 object-contain mix-blend-overlay opacity-90 ZapSol-respirando"
           />
         </a>
       </div>
