4a20eee8199dfca853c63a3c7f51e0101336bd73
Manuel <samusnow@hotmail.com>
2025-12-02 03:31:19 +0100

    Auras Héroes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -173,7 +173,7 @@ export default function HeroSection() {
           <img
             src="/assets/MonkeSol.png"
             alt="MonkeSol Hero"
-            className="relative z-10 h-64 w-auto md:h-72 object-contain mix-blend-overlay opacity-90 logo-respirando"
+            className="relative z-10 h-64 w-auto md:h-72 object-contain mix-blend-overlay opacity-90 symbol-monkesol"
           />
         </div>
       </div>
