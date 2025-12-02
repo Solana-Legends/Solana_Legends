ffcac71cd4a2877620d2d64e7010a6cf6b316c08
Manuel <samusnow@hotmail.com>
2025-12-02 03:43:00 +0100

    Auras ZapSol y MonkeSol

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -168,7 +168,7 @@ export default function HeroSection() {
       <div className="absolute bottom-0 left-0 mb-[4rem] ml-[2rem] animate-in fade-in slide-in-from-left duration-1000">
         <div className="relative z-10 flex justify-center items-center w-80 h-80">
           {/* Aura dorada pulsante */}
-          <div className="absolute w-80 h-80 rounded-full blur-3xl aura-pulsante-gold-strong pointer-events-none z-0"></div>
+          <div className="absolute w-80 h-80 rounded-full blur-3xl symbol-monkesol-aura pointer-events-none z-0"></div>
           {/* Imagen del héroe MonkeSol */}
           <img
             src="/assets/MonkeSol.png"
--- src/components/ProgressSection.tsx
+++ src/components/ProgressSection.tsx
@@ -150,7 +150,7 @@ export default function ProgressSection() {
       <div className="relative flex justify-end items-center mt-[-6rem] mr-[-24rem] mb-[-16rem] translate-y-[-16rem] animate-in fade-in slide-in-from-right duration-1000">
         <div className="relative z-10 flex justify-center items-center w-80 h-80">
           {/* Aura dorada pulsante */}
-          <div className="absolute w-80 h-80 rounded-full blur-3xl aura-pulsante-gold-strong pointer-events-none z-0"></div>
+          <div className="absolute w-80 h-80 rounded-full blur-3xl symbol-zapsol-aura pointer-events-none z-0"></div>
           {/* Imagen del héroe ZapSol */}
           <img
             src="/assets/ZapSol.png"
