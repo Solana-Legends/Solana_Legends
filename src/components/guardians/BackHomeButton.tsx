bccae0ae7e988aa6f2111b88573157aa9459ea79
Manuel <samusnow@hotmail.com>
2025-12-03 10:12:17 +0100

    prueba

--- src/components/guardians/BackHomeButton.tsx
+++ src/components/guardians/BackHomeButton.tsx
@@ -1,18 +1,14 @@
+// components/guardians/BackHomeButton.tsx
 import { Link } from "react-router-dom";
 import { useLanguage } from "@/contexts/LanguageContext";
-import { useState } from "react";
 
 export default function BackHomeButton() {
   const { t } = useLanguage();
-  const [exit, setExit] = useState(false);
 
   return (
     <Link
       to="/"
-      onClick={() => setExit(true)}
-      className={`flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white font-semibold px-5 py-3 rounded-lg text-lg transition ${
-        exit ? "animate-slideOutLeftGlow" : "animate-slideInLeftGlow"
-      }`}
+      className="inline-flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white font-semibold px-5 py-3 rounded-lg text-lg transition relative"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
