654a8e3e06cd60262fa36167e46ea08cc15d2e74
Manuel <samusnow@hotmail.com>
2026-04-14 04:28:02 +0200

    v

--- src/pages/HallOfHeroesPage.tsx
+++ src/pages/HallOfHeroesPage.tsx
@@ -1,24 +1,24 @@
-// src/pages/HallOfHeroesPage.tsx
 import HallOfHeroesPortal from "../components/heroes/HallOfHeroesPortal";
 import HeroDocumentary from "../components/heroes/HeroDocumentary";
 import HeroesGallery from "../components/heroes/HeroesGallery";
-import LanguageSwitcher from "@/components/LanguageSwitcher";
+import LanguageSwitcher from "@/components/LanguageSwitcher"; // Recomendado para que puedan cambiar el idioma aquí también
 
 export default function HallOfHeroesPage() {
   return (
     <main className="min-h-screen bg-[#0F0B1E] overflow-x-hidden selection:bg-purple-500/30">
       
+      {/* Selector de idioma flotante por si el usuario quiere leer el lore en otro idioma */}
       <div className="absolute top-4 right-4 z-50">
         <LanguageSwitcher />
       </div>
 
-      {/* 1. La Entrada Mística */}
+      {/* 1. El Atrio / Entrada */}
       <HallOfHeroesPortal />
 
-      {/* 2. El Documental (Evento Principal) */}
+      {/* 2. El Puente Narrativo (Vídeo Central) */}
       <HeroDocumentary />
 
-      {/* 3. El Gran Contenedor (Vídeos extra + Salas + Timeline) */}
+      {/* 3. Las Cámaras de Contención y Cronología */}
       <HeroesGallery />
 
     </main>
